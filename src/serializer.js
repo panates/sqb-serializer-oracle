eabd1d940657819823f2df65a67a0d95360d1988
Eray Hanoglu <e.hanoglu@panates.com>
2017-06-02 15:25:38 +0300

    - Fix invalid returning for empty tables name. "from dual" changed to "dual"

--- src/serializer.js
+++ src/serializer.js
@@ -11,66 +11,69 @@ const {Serializer} = require('sqb');
 
 class OracleSerializer extends Serializer {
 
-    constructor(config) {
-        super(config);
-        this.dialect = 'oracle';
-    }
+  constructor(config) {
+    super(config);
+    this.dialect = 'oracle';
+    this.reservedWords = this.reservedWords.concat(['comment', 'dual']);
+  }
 
-    //noinspection JSUnusedGlobalSymbols
-    /**
-     * @override
-     */
-    _serializeSelect(obj) {
-        let sql = super._serializeSelect(obj);
-        const prettyPrint = this.prettyPrint,
-            limit = obj._limit,
-            offset = obj._offset;
-        if (limit || offset) {
-            sql =
-                'select ' + (obj._alias ? obj._alias + '.' : '') + '* from (select rownum row$number, t.* from (' +
-                (prettyPrint ? '\n  ' : '') +
-                sql +
-                (prettyPrint ? '\n' : '') +
-                ') t)' + (obj._alias ? ' ' + obj._alias : '') +
-                (prettyPrint ? '\nwhere' : ' where') +
-                (offset ? ' row$number >= ' + offset : '') +
-                (limit ? (offset ? ' and' : '') + ' row$number <= ' + (limit + (offset || 0)) : '');
-        }
-        return sql;
+  //noinspection JSUnusedGlobalSymbols
+  /**
+   * @override
+   */
+  _serializeSelect(obj) {
+    let sql = super._serializeSelect(obj);
+    const prettyPrint = this.prettyPrint;
+    const limit = obj._limit;
+    const offset = obj._offset;
+    if (limit || offset) {
+      sql =
+          'select ' + (obj._alias ? obj._alias + '.' : '') +
+          '* from (select rownum row$number, t.* from (' +
+          (prettyPrint ? '\n  ' : '') +
+          sql +
+          (prettyPrint ? '\n' : '') +
+          ') t)' + (obj._alias ? ' ' + obj._alias : '') +
+          (prettyPrint ? '\nwhere' : ' where') +
+          (offset ? ' row$number >= ' + offset : '') +
+          (limit ? (offset ? ' and' : '') + ' row$number <= ' +
+              (limit + (offset || 0)) : '');
     }
+    return sql;
+  }
 
-    //noinspection JSUnusedGlobalSymbols
-    /**
-     * @override
-     */
-    _serializeTablesNames(tables) {
-        return super._serializeTablesNames(tables) || 'from dual';
-    }
+  //noinspection JSUnusedGlobalSymbols
+  /**
+   * @override
+   */
+  _serializeTablesNames(tables) {
+    return super._serializeTablesNames(tables) || 'dual';
+  }
 
-    //noinspection JSUnusedGlobalSymbols
-    /**
-     * @override
-     */
-    _serializeCondition(item) {
-        let s = super._serializeCondition(item);
-        if (!item.isRaw) {
-            s = s.replace(/!= ?null/g, 'is not null')
-                .replace(/<> ?null/g, 'is not null')
-                .replace(/= ?null/g, 'is null');
-        }
-        return s;
+  //noinspection JSUnusedGlobalSymbols
+  /**
+   * @override
+   */
+  _serializeCondition(item) {
+    let s = super._serializeCondition(item);
+    if (!item.isRaw) {
+      s = s.replace(/!= ?null/g, 'is not null')
+          .replace(/<> ?null/g, 'is not null')
+          .replace(/= ?null/g, 'is null');
     }
+    return s;
+  }
 
-    //noinspection JSUnusedGlobalSymbols
-    /**
-     * @override
-     */
-    _serializeDateValue(date) {
-        const s = super._serializeDateValue(date);
-        return s.length <= 12 ?
-            'to_date(' + s + ", 'yyyy-mm-dd')" :
-            'to_date(' + s + ", 'yyyy-mm-dd hh24:mi:ss')"
-    }
+  //noinspection JSUnusedGlobalSymbols
+  /**
+   * @override
+   */
+  _serializeDateValue(date) {
+    const s = super._serializeDateValue(date);
+    return s.length <= 12 ?
+        'to_date(' + s + ', \'yyyy-mm-dd\')' :
+        'to_date(' + s + ', \'yyyy-mm-dd hh24:mi:ss\')';
+  }
 }
 
 Serializer.register('oracle', OracleSerializer);
